484ec0c3f1caef42a147b30b7423780c5b94ac6e
Thiago Mateus <thiago.mateus0707@gmail.com>
2023-07-25 19:22:48 -0300

    Initial commit

---
+++ src/modules/Products/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import Categories from '../../components/Categories'
+import ProductCard from '../../components/ProductCard'
+
+const Products = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products')
+      const data = await response.json()
+      console.log(data)
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
+
+  return (
+    <div>
+      <Categories/>
+      <div className="flex flex-col text-center w-full mb-20">
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">OUTLET PREMIUM</h2>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">TODOS OS PRODUTOS</h1>
+      </div>
+      {
+        products.length > 0 ?
+        <ProductCard products={products}/>
+        :
+        <div>CARREGANDO.....</div>
+      }
+    </div>
+  )
+}
+
+export default Products
